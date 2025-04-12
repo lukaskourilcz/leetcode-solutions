e2368171f42393452b25318daff129f566b7cff5
Lukas <58434653+lukaskourilcz@users.noreply.github.com>
2025-04-12 11:32:42 +0900

    Time: 0 ms (100%), Space: 55.9 MB (15.68%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+var addBinary = function (a, b) {
+    let result = '';
+    let carry = 0;
+    let i = a.length - 1;
+    let j = b.length - 1;
+
+    while (i >= 0 || j >= 0 || carry) {
+        const bitA = i >= 0 ? parseInt(a[i--]) : 0;
+        const bitB = j >= 0 ? parseInt(b[j--]) : 0;
+
+        const sum = bitA + bitB + carry;
+        result = (sum % 2) + result;
+        carry = Math.floor(sum / 2);
+    }
+
+    return result;
+}
