0fb82bb47f3f1596c0662b8bd018b188c76a0a7d
Lukas <58434653+lukaskourilcz@users.noreply.github.com>
2025-05-10 17:29:07 +0900

    Time: 10 ms (36.84%), Space: 60.8 MB (38.97%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+var convert = function(s, numRows) {
+  if (numRows === 1 || s.length <= numRows) return s;
+
+  const rows = Array(Math.min(numRows, s.length)).fill('').map(() => '');
+  let currRow = 0;
+  let goingDown = false;
+
+  for (let c of s) {
+    rows[currRow] += c;
+
+    if (currRow === 0 || currRow === numRows - 1) goingDown = !goingDown;
+
+    currRow += goingDown ? 1 : -1;
+  }
+
+  return rows.join('');
+};
