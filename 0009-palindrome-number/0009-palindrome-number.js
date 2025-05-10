080173a1a0b825e2d3f5bdcd8542f44d03df9edd
Lukas <58434653+lukaskourilcz@users.noreply.github.com>
2025-05-10 18:07:15 +0900

    Time: 13 ms (20.03%), Space: 65.7 MB (7.34%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    let str = x.toString()
+    if(str === str.split('').reverse().join('')){
+    return true}
+    return false
+};
