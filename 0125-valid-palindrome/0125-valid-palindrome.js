5277a7a2db4a01ee6fb40b698357445a320ca386
Lukas <58434653+lukaskourilcz@users.noreply.github.com>
2025-05-17 23:36:31 +0900

    Time: 3 ms (85.98%), Space: 58.2 MB (44.31%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function (s) {
+    const filtered = s.replace(/[^a-z0-9]/gi, '').toLowerCase();
+    const reversed = filtered.split('').reverse().join('');
+    return filtered === reversed;
+};
